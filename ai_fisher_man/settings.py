c476f032b19eb81062663acd13e5188581371d43
Black Dreamer <blackdreamer007@gmail.com>
2026-04-18 05:44:38 -0700

    mysql connected

--- ai_fisher_man/settings.py
+++ ai_fisher_man/settings.py
@@ -84,8 +84,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'KqHhkxbOEZHzypjZinKmpSFnnrRUCIzs',
+        'HOST': 'junction.proxy.rlwy.net',
+        'PORT': '25464',
     }
 }
 
